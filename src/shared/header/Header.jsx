559931af0dee1c392a9f425971d8f45627668417
nando-castro <fernando.castro10.master@gmail.com>
2022-09-14 13:41:36 -0300

    feat: header home

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,13 +1,62 @@
-import { Container, Icon, Text } from "./styles";
+import {
+  Container,
+  ContainerSearch,
+  ContainerTop,
+  ContainerUser,
+  Exit,
+  Icon,
+  Logo,
+  Text,
+} from "./styles";
 import logo from "../../assets/imgs/Icon.png";
+import { IoLogOutOutline } from "react-icons/io5";
+import { useAuth } from "../../context/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  function logout() {
+    navigate("/");
+    localStorage.clear();
+    window.location.reload();
+  }
   return (
-    <Container>
-      <Icon src={logo} alt="icon" width="61px" height="61px" />
-      <Text>
-        Repo<p>Provas</p>
-      </Text>
-    </Container>
+    <>
+      {user === null ? (
+        <Container>
+          <Icon src={logo} alt="icon" width="61px" height="61px" />
+          <Text>
+            Repo<p>Provas</p>
+          </Text>
+        </Container>
+      ) : (
+        <Container>
+          <ContainerTop>
+            <ContainerUser>
+              <Logo>
+                <Icon src={logo} alt="icon" width="61px" height="61px" />
+                <Text>
+                  Repo<p>Provas</p>
+                </Text>
+              </Logo>
+              <Exit>
+                <IoLogOutOutline className="icon" onClick={logout} />
+              </Exit>
+            </ContainerUser>
+            <ContainerSearch>
+              <input
+                type="password"
+                placeholder="Pesquise por disciplina"
+                //value={userLogin.password}
+                name="password"
+                //onChange={changeInput}
+              />
+            </ContainerSearch>
+          </ContainerTop>
+        </Container>
+      )}
+    </>
   );
 }
--- src/shared/header/styles.js
+++ src/shared/header/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100px;
+  height: 250px;
 
   display: flex;
   align-items: center;
@@ -11,6 +11,52 @@ export const Container = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+`;
+
+export const ContainerUser = styled.div`
+  width: 100%;
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ContainerTop = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Logo = styled.div`
+  width: 100%;
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+  justify-content: left;
+
+  padding-left: 60px;
+`;
+export const Exit = styled.div`
+  width: 100%;
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+  justify-content: right;
+
+  padding-right: 30px;
+
+  cursor: pointer;
+
+  .icon {
+    font-size: 50px;
+  }
 `;
 
 export const Icon = styled.img`
@@ -35,5 +81,37 @@ export const Text = styled.div`
     font-size: 36px;
     line-height: 45px;
     color: #3f61d7;
+  }
+`;
+
+export const ContainerSearch = styled.div`
+  width: 100%;
+  height: 150px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-bottom: 1px solid #c4c4c4;
+
+  input {
+    width: 464px;
+    height: 56px;
+
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.15px;
+
+    color: rgba(0, 0, 0, 0.6);
+
+    ::placeholder {
+      padding-left: 12px;
+    }
   }
 `;
